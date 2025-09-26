732e03c29ca5b36a9fc6e12f0afa1cf5308e368a
Tripix_JCI0 <redota.jadecarlo.dll@gmail.com>
2025-09-26 12:39:51 +0800

    Fix templates structure, move base.html

--- facecapstone/settings.py
+++ facecapstone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=kj297h2q4sj+hf&c0^(&n6k$q8fw1)85^nufue7ly-c^03*d4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic puts files
+STATIC_ROOT = BASE_DIR / 'staticfiles' # where collectstatic puts files
 STATICFILES_DIRS = [BASE_DIR / 'matches' / 'static']  # your app static files
 
 MEDIA_URL = '/media/'
